d01b80b1f9e12511e87fc7dbb7e33542b36dce64
John James <john.james.ai.studio@gmail.com>
2023-02-05 07:42:12 -0500

    as_sparse, pearson lab

--- recsys/neighborhood/metric.py
+++ recsys/neighborhood/metric.py
@@ -11,14 +11,12 @@
 # URL        : https://github.com/john-james-ai/recsys-deep-learning-udemy                         #
 # ------------------------------------------------------------------------------------------------ #
 # Created    : Saturday February 4th 2023 05:26:17 am                                              #
-# Modified   : Saturday February 4th 2023 07:57:42 pm                                              #
+# Modified   : Sunday February 5th 2023 07:42:12 am                                                #
 # ------------------------------------------------------------------------------------------------ #
 # License    : MIT License                                                                         #
 # Copyright  : (c) 2023 John James                                                                 #
 # ================================================================================================ #
-from typing import Union
 from scipy import sparse
-import numpy as np
 
 from recsys.neighborhood.base import Metric
 
@@ -27,42 +25,71 @@ from recsys.neighborhood.base import Metric
 class CosineSimilarity(Metric):
     """Creates a Cosine similarity matrix."""
 
-    def __init__(self, name: str = "Cosine_similarity") -> None:
+    def __init__(self, name: str = "cosine_similarity") -> None:
         super().__init__(name=name)
 
     def __call__(
-        self,
-        X: Union[np.ndarray, sparse.csr_matrix],
-        Y: Union[np.ndarray, sparse.csr_matrix] = None,
-        dimension: str = 'user',
-        return_sparse: bool = True,
-    ) -> Union[np.ndarray, sparse.csr_matrix]:
+        self, X: sparse.csr_matrix, Y: sparse.csr_matrix = None, dimension: str = "user"
+    ) -> sparse.csr_matrix:
         """Computes Cosine similarity between samples X and Y
 
         Args:
-            X (np.ndarray, sparse.csr_matrix): array or sparse matrix
-            Y (np.ndarray, sparse.csr_matrix): array, sparse matrix or None
-            return_sparse (bool): Whether to return an array or sparse matrix.
+            X (sparse.csr_matrix): array or sparse matrix
+            Y (sparse.csr_matrix): array, sparse matrix or None
+            dimension (str): Either 'user' or 'item'
 
         """
         X, Y = self._check_input(X, Y)
-
-        if isinstance(X, np.ndarray):
-            C = self._cosine_arrays(X, Y)
+        if "u" in dimension:
+            return self._cosine_user(X, Y)
         else:
-            C = self._cosine_sparse(X, Y)
+            return self._cosine_item(X, Y)
 
-        if return_sparse and isinstance(C, np.ndarray):
-            C = sparse.csr_matrix(C)
-
-        return C
-
-    def _cosine_arrays(self, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
-        """Computes Cosine similarity for numpy arrays"""
-        XY = X.dot(Y.T)
-        return self._normalize(XY, norm="l2", axis=1)
-
-    def _cosine_sparse(self, X: sparse.csr_matrix, Y: sparse.csr_matrix) -> sparse.csr_matrix:
-        """Computes Cosine similarity for sparse csr matrices."""
+    def _cosine_user(self, X: sparse.csr_matrix, Y: sparse.csr_matrix) -> sparse.csr_matrix:
+        """Computes user cosign similarity, normalizing over all items"""
         XY = X.dot(Y.transpose())
         return self._normalize(XY, norm="l2", axis=1)
+
+    def _cosine_item(self, X: sparse.csr_matrix, Y: sparse.csr_matrix) -> sparse.csr_matrix:
+        """Computes item cosign similarity, normalizing over all users"""
+        XY = X.transpose().dot(Y)
+        return self._normalize(XY, norm="l2", axis=0)
+
+
+# ------------------------------------------------------------------------------------------------ #
+class PearsonSimilarity(Metric):
+    """Creates a Pearson similarity matrix.
+
+    Computes the pairwise user or item similarities considering only ratings for the
+    common items.
+    """
+
+    def __init__(self, coname: str = "pearson_similarity") -> None:
+        super().__init__(name=name)
+
+    def __call__(
+        self, X: sparse.csr_matrix, Y: sparse.csr_matrix = None, dimension: str = "user"
+    ) -> sparse.csr_matrix:
+        """Computes Cosine similarity between samples X and Y
+
+        Args:
+            X (sparse.csr_matrix): array or sparse matrix
+            Y (sparse.csr_matrix): array, sparse matrix or None
+            dimension (str): Either 'user' or 'item'
+
+        """
+        X, Y = self._check_input(X, Y)
+        if "u" in dimension:
+            return self._pearson_user(X, Y)
+        else:
+            return self._pearson_item(X, Y)
+
+    def _pearson_user(self, X: sparse.csr_matrix, Y: sparse.csr_matrix) -> sparse.csr_matrix:
+        """Computes user cosign similarity, normalizing over all items"""
+        XY = X.dot(Y.transpose())
+        return self._normalize(XY, norm="l2", axis=1)
+
+    def _pearson_item(self, X: sparse.csr_matrix, Y: sparse.csr_matrix) -> sparse.csr_matrix:
+        """Computes item cosign similarity, normalizing over all users"""
+        XY = X.transpose().dot(Y)
+        return self._normalize(XY, norm="l2", axis=0)
